1b05d26d524b807740928e7b4039c05467629965
assafrbi <73109803+assafrbi@users.noreply.github.com>
2020-12-19 10:32:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-from game.battle import *
-from classes.inventory import *
+from game.battle import Battle
 
-# create players
-
-assaf = Person("assaf", 3400, 35, 30, 10,1)
-liat = Person("liat", 1400, 55, 30, 10,1)
-
-enemy = Person("covid19", 5100, 25, 30, 1500,1)
-
-players = [assaf, liat]
-
-running = True
-
-print("****let the battle begin****")
-while running:
-    for player_one in players:
-        cycle = Battle(enemy,players)
+if __name__ == "__main__":
+    running = True
+    while running:
+        cycle = Battle()
         running = cycle.start_fight()
 
-print("************")
-print("End of Game")
-print("************")
+    print("************")
+    print("End of Game")
+    print("************")
