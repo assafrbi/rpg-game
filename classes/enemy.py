1af8a73c044b65ead4388706c45ee4ccc22c770b
assafrbi <73109803+assafrbi@users.noreply.github.com>
2020-12-19 10:37:22 +0200

    Update enemy.py

--- classes/enemy.py
+++ classes/enemy.py
@@ -1,76 +1,45 @@
-import random
 
+from classes.player import Person
 
-def generate_damage(self):
-    atk_low = self.atk - 10
-    atk_high = self.atk + 10
-    return random.randrange(atk_low, atk_high)
-
-
-def take_damage(self, dmg):
-    self.hp -= dmg
-    if self.hp <= 0:
-        self.hp = 0
-
-
-def get_hp(self):
-    return self.hp
-
-
-def is_dead(self):
-    if self.get_hp() == 0:
-        self.live = 0
-        return True
-    else:
-        return False
-
-
-def get_name(self):
-    return self.name
-
-
-def set_hp(self, i):
-    self.hp += i
-    if self.hp > self.max_hp:
-        self.hp = self.max_hp
-
-
-def set_atk(self, i):
-    self.atk += i
+monster_number = 1
+soldier_number = 1
+leader_number = 1
 
 
 class Monster:
-    def __init__(self, name):
-        self.live = 1
-        self.hp = 100
-        self.max_hp = 100
-        self.atk = 25
-        self.name = name
-        self.df = 0
-        self.actions = ["Attack", "sleep"]
+    def __init__(self):
+        global monster_number
+        self.player = Person("Monster_" + str(monster_number), 300, 20, 20, 5, 15, 1)
+        monster_number += 1
 
-    def action(self):
-        if get_hp(self) < 25:
-            return "Sleep"
-        else:
-            return "Attack"
+    def get_player(self):
+        return self.player
+
+    def get_name(self):
+        return self.get_player().get_name()
 
 
 class Soldier:
-    def __init__(self, name):
-        self.live = 1
-        self.hp = 100
-        self.max_hp = 100
-        self.atk = 10
-        self.name = name
-        self.df = 15
-        self.actions = ["Attack", "Guard"]
+    def __init__(self):
+        global soldier_number
+        self.player = Person("Soldier_" + str(soldier_number), 200, 25, 25, 5, 15, 1)
+        soldier_number += 1
+
+    def get_player(self):
+        return self.player
+
+    def get_name(self):
+        return self.get_player().get_name()
 
 
 class Leader:
-    def __init__(self, name):
-        self.live = 1
-        self.hp = 100
-        self.max_hp = 100
-        self.atk = 10
-        self.name = name
+    def __init__(self):
+        global leader_number
+        self.player = Person("Monster_" + str(leader_number), 300, 35, 25, 7, 20, 1)
+        leader_number += 1
+
+    def get_player(self):
+        return self.player
+
+    def get_name(self):
+        return self.get_player().get_name()
